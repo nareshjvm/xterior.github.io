66cdba41c65a36a2d93d26b33455f60a6b16da2a
Gnana Sudhakar Ch <ch.gnana.sudhakar@gmail.com>
2018-05-09 11:36:12 +0530

    oururlnow

--- sw.js
+++ sw.js
@@ -42,6 +42,6 @@ self.addEventListener('notificationclick', function(event) {
 	  event.notification.close();
 
 	  event.waitUntil(
-	    clients.openWindow('https://developers.google.com/web/')
+	    clients.openWindow('http://webapp-180507044224.azurewebsites.net/')
 	  );
 });
